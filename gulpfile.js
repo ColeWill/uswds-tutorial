f6c11754a3893a2ac1b08ecc6b87f6a1f3c3ea5c
cowilliams_deloitte <cowilliams@deloitte.com>
2025-01-15 11:25:20 -0700

    completed tutorial part one

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ const uswds = require('@uswds/compile');
 uswds.settings.version  = 3;
 
 uswds.paths.dist.theme = './_theme';
+uswds.paths.src.projectSass = './assets/css';
 
 exports.init = uswds.init;
 exports.compile = uswds.compile;
